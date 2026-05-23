e11aca8d8a4924c0937a09035cf8f244dbffb36d
pathikroy86 <pathik.roy86@gmail.com>
2026-05-23 07:34:04 +0600

    auth error

--- web/lib/auth-client.js
+++ web/lib/auth-client.js
@@ -4,7 +4,8 @@ export const authClient = createAuthClient({
   baseURL: "/api/auth"
 });
 
-export const authClient = new Proxy({}, {
-  get(target, prop) {
-    return initAuthClient()[prop];
-  }
+// export const authClient = new Proxy({}, {
+//   get(target, prop) {
+//     return initAuthClient()[prop];
+//   }
+// })
